6fa6c079a9c3c6642c47968d06406e9cf1dc1da8
Sevi <correosevi@gmail.com>
2022-06-10 11:38:54 +0100

    Apply styles to card component

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,14 +1,35 @@
-import React from "react";
-import css from "./productCard.module.css";
+import React from 'react';
+import css from './productCard.module.css';
+import Favorite from '../../favorite.svg';
 
 function ProductCard({ product }) {
   return (
     <div className={css.productCard}>
+      <div className={css.imageContainer}>
+        <img className={css.image} src={product.image} alt={product.title} />
+      </div>
+      <div className={css.priceContainer}>
+        <span className={css.price}>£{product.price}</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="icon icon-tabler icon-tabler-heart"
+          width="36"
+          height="36"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="#e31c47"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+        </svg>
+      </div>
+
       <h2 className={css.productTitle}>{product.title}</h2>
-      <span className={css.price}>{product.price}</span>
+      <p className={css.category}>Category: {product.category}</p>
       <p className={css.description}>{product.description}</p>
-      <img className={css.image} src={product.image} alt={product.title} />
-      <span className={css.category}>Category: {product.category}</span>
     </div>
   );
 }
