43208dbc45c14107d5689529a20230842353f6bb
D20CM <scottbrown500@hotmail.com>
2022-06-10 10:14:21 +0100

    add productInCard component

---
+++ src/components/ProductInCart/ProductInCart.js
@@ -0,0 +1,14 @@
+import React from "react";
+import css from "./productInCart.module.css";
+
+function ProductInCart(product) {
+  return (
+    <div className={css.productInCart}>
+      <img className={css.image} src={product.image} alt={product.title} />
+      <h4 className={css.title}>{product.title}</h4>
+      <span className={css.price}>£{product.price}</span>
+    </div>
+  );
+}
+
+export default ProductInCart;
